c37f949e31b0f3fd7ed750dee139d30ce01bc327
uladzislauRamanchanka <romanchenko.vladyslav@yandex.by>
2020-07-27 10:59:54 +0300

    task 1 (core concepts)
    - Install blank create-react-app
    application
    
    - Render blank message (Hello
    World) with React
    
    - Use at least 2 methods of creating
    react components
    
    - Use all methods which mentioned
    in task, to create React
    components, installed and used all
    extensions

---
+++ Task 1 Core concepts/my-app/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+
+const createElement = React.createElement(
+    'h1',
+    null,
+    'Hello World! (Created within React.createElement)'
+);
+
+const FunctionalComponent = () => <h1>Hello World! (Created within functional component)</h1>;
+
+class Component extends React.Component {
+  render() {
+      return <h1>Hello World! (Created within React.Component)</h1>
+  }
+}
+
+class PureComponent extends React.PureComponent {
+  render() {
+      return <h1>Hello World! (Created within React.PureComponent)</h1>
+  }
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      {createElement}
+      <FunctionalComponent/>
+      <Component/>
+      <PureComponent/>
+    </div>
+  );
+}
+
+export default App;
