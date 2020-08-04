2810c5790634c00e88b48b50c753a43a400d7ff0
uladzislauRamanchanka <romanchenko.vladyslav@yandex.by>
2020-08-05 01:14:11 +0300

    add plugins and set variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,63 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const isDev = process.env.NODE_ENV  === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetsPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config;
+}
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        main: ['@babel/polyfill','./index.js']
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "bundle.js"
+      },
+      resolve: {
+        extensions: ['.js', '.jsx', '.png']
+      },
+      optimization: optimization(),
+      devServer: {
+        port: 5500,
+        hot: isDev
+    },
+    devtool: isDev ? 'source-map': '',
+      module: {
+        rules: [
+            {
+                test: /\.js|.jsx$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+              },
+        ]
+      },
+      plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin()
+      ]
+}
